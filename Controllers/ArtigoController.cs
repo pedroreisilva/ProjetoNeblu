fe548940b5879a44cb01ff27ebb166b4d0f95e40
pedroreisilva <48354011+pedroreisilva@users.noreply.github.com>
2020-07-08 15:12:56 +0100

    CRUD done

---
+++ Controllers/ArtigoController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GestaoArtigos.Models;
+
+namespace GestaoArtigos.Controllers
+{
+    public class ArtigoController : Controller
+    {
+        // GET: Artigo/Index
+        public ActionResult Index()
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+            return View(dbModel.tb_artigos.ToList());
+            }
+        }
+
+        // GET: Artigo/Details/5
+        public ActionResult Details(int id)
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+                return View(dbModel.tb_artigos.Where(x => x.codigo == id).FirstOrDefault());
+            }
+        }
+
+        // GET: Artigo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Artigo/Create
+        [HttpPost]
+        public ActionResult Create(tb_artigos artigo)
+        {
+            try
+            {
+                using (DbModels dbModel = new DbModels())
+                {
+                    dbModel.tb_artigos.Add(artigo);
+                    dbModel.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Artigo/Edit/5
+        public ActionResult Edit(int id)
+        {
+            tb_artigos artigoModel = new tb_artigos();  
+            using (DbModels dbModel = new DbModels())
+            {
+                artigoModel = dbModel.tb_artigos.Where(x => x.codigo == id).FirstOrDefault();
+            }
+            return View(artigoModel);
+        }
+
+        // POST: Artigo/Edit/5
+        [HttpPost]
+        public ActionResult Edit(tb_artigos artigoModel)
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+                dbModel.Entry(artigoModel).State = EntityState.Modified;
+                dbModel.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: Artigo/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+                return View(dbModel.tb_artigos.Where(x => x.codigo == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Artigo/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                using (DbModels dbModel = new DbModels())
+                {
+                    tb_artigos artigo = dbModel.tb_artigos.Where(x => x.codigo == id).FirstOrDefault();
+                    dbModel.tb_artigos.Remove(artigo);
+                    dbModel.SaveChanges();                
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
