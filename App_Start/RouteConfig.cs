36ebe3e37eff87389d687f514260bda31371e220
pedroreisilva <48354011+pedroreisilva@users.noreply.github.com>
2020-07-20 11:15:28 +0100

    Route changes

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -15,8 +15,8 @@ namespace GestaoArtigos
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{controller}/{action}",
+                defaults: new { controller = "Account", action = "Login"}
             );
         }
     }
