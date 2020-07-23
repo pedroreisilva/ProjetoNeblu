4c73c69df48d7c13f04bd376ea339b4cad5400bc
pedroreisilva <48354011+pedroreisilva@users.noreply.github.com>
2020-07-23 11:36:49 +0100

    Route changes

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -14,10 +14,17 @@ namespace GestaoArtigos
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}",
-                defaults: new { controller = "Account", action = "Login"}
+                name: "Login",
+                url: "Login",
+                defaults: new { controller = "Account", action = "Login" }
             );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
         }
     }
 }
